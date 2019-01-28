c273f7e323a0b906b50a6cc6eca9b9d02e3acdb6
The Buildbot <buildbot@angular2buch.de>
2019-01-28 15:37:21 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/cd298cab7eb05f6ddaade5c3b76a96aac3a443b1
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/2849

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { DateValueAccessorModule } from 'angular-date-value-accessor';
 
